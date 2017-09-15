31db6b2254bf362f09736ad438d33e6c5379a501
Muhammad Usman Irfan <mirfan@netpace.com>
2017-09-15 13:02:05 +0500

    foreign key issue resolved

--- api/user/signup.js
+++ api/user/signup.js
@@ -1,5 +1,6 @@
 module.exports = function(user,res){
     var MongoClient = require('mongodb').MongoClient;
+    var ObjectID = require('mongodb').ObjectID;
     var path = require('path');
     var root_dir = __dirname + '/../../';
     var config = require(root_dir + 'config');
@@ -7,10 +8,12 @@ module.exports = function(user,res){
     var dto = {success:true,error:[],status:200};
     var response = res;
 
+    user.tenantId = new ObjectID(user.tenantId);
+
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var query = {
-            tenantId: user.tenantId,
+            tenantId: new ObjectID(user.tenantId),
             username:user.username
         }
 
