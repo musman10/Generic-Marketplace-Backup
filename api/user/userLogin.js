ad2042c3d2e3bbef0aebd3797a3439d19d877361
MARKETPLACE1-PC\marketplace1 <sbano@netpace.com>
2017-09-18 12:23:53 +0500

    configuration changes

--- api/user/userLogin.js
+++ api/user/userLogin.js
@@ -5,7 +5,10 @@ var mongo = require('mongodb');
 
 module.exports = function(loginUser,response){
     var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/apptest";
+    var path = require('path');
+    var root_dir = __dirname + '/../../';
+    var config = require(root_dir + 'config');
+    var url = config.dbConnection.url;
     var dto = {success : true,
     error : [],
     status : 200};
