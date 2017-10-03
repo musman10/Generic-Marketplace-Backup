88d8761330293da235e1cc4cd5de5a030aca7cbd
Saira Bano <sbano@netpace.com>
2017-10-03 10:04:21 +0500

    Static charts for dashboard

--- src/controller/home/tenantUserHomeController.js
+++ src/controller/home/tenantUserHomeController.js
@@ -5,4 +5,203 @@ angular.module('angularApp')
             message2 : 'developing for testing',
             message3 : tenantLoginService.getPrivate()
         };
+        debugger;
+        $scope.myChartObject = {
+            "type": "ColumnChart",
+            "displayed": false,
+            "data": {
+                "cols": [
+                    {
+                        "id": "month",
+                        "label": "Month",
+                        "type": "string",
+                        "p": {}
+                    },
+                    {
+                        "id": "laptop-id",
+                        "label": "Laptop",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "desktop-id",
+                        "label": "Desktop",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "server-id",
+                        "label": "Server",
+                        "type": "number",
+                        "p": {}
+                    },
+                    {
+                        "id": "cost-id",
+                        "label": "Shipping",
+                        "type": "number"
+                    }
+                ],
+                "rows": [
+                    {
+                        "c": [
+                            {
+                                "v": "January"
+                            },
+                            {
+                                "v": 19,
+                                "f": "42 items"
+                            },
+                            {
+                                "v": 12,
+                                "f": "Ony 12 items"
+                            },
+                            {
+                                "v": 7,
+                                "f": "7 servers"
+                            },
+                            {
+                                "v": 4
+                            }
+                        ]
+                    },
+                    {
+                        "c": [
+                            {
+                                "v": "February"
+                            },
+                            {
+                                "v": 13
+                            },
+                            {
+                                "v": 1,
+                                "f": "1 unit (Out of stock this month)"
+                            },
+                            {
+                                "v": 12
+                            },
+                            {
+                                "v": 2
+                            }
+                        ]
+                    },
+                    {
+                        "c": [
+                            {
+                                "v": "March"
+                            },
+                            {
+                                "v": 24
+                            },
+                            {
+                                "v": 5
+                            },
+                            {
+                                "v": 11
+                            },
+                            {
+                                "v": 6
+                            }
+                        ]
+                    }
+                ]
+            },
+            "options": {
+                "title": "Sales per month",
+                "isStacked": "true",
+                "fill": 20,
+                "displayExactValues": true,
+                "vAxis": {
+                    "title": "Sales unit",
+                    "gridlines": {
+                        "count": 10
+                    }
+                },
+                "hAxis": {
+                    "title": "Date"
+                }
+            },
+            "formatters": {}
+        }
+
+        $scope.myPieChartObject = {};
+
+        $scope.myPieChartObject.type = "PieChart";
+
+        $scope.onions = [
+            {v: "Onions"},
+            {v: 3},
+        ];
+
+        $scope.myPieChartObject.data = {"cols": [
+            {id: "t", label: "Topping", type: "string"},
+            {id: "s", label: "Slices", type: "number"}
+        ], "rows": [
+            {c: [
+                {v: "Mushrooms"},
+                {v: 3},
+            ]},
+            {c: $scope.onions},
+            {c: [
+                {v: "Olives"},
+                {v: 31}
+            ]},
+            {c: [
+                {v: "Zucchini"},
+                {v: 1},
+            ]},
+            {c: [
+                {v: "Pepperoni"},
+                {v: 2},
+            ]}
+        ]};
+
+        $scope.myPieChartObject.options = {
+            'title': 'How Much Pizza I Ate Last Night'
+        };
+
+
+
+        $scope.buildGridModel = function (tileTmpl) {
+            var it, results = [];
+
+            for (var j = 0; j < 5; j++) {
+
+                it = angular.extend({}, tileTmpl);
+                //it.icon = it.icon + (j + 1);
+                it.title = it.title + (j + 1);
+                it.span = {row: 1, col: 1};
+
+                switch (j + 1) {
+
+                    case 1:
+                        it.background = "lightPurple";
+                        break;
+                    case 2:
+                        it.background = "yellow";
+                        break;
+                    case 3:
+                        it.background = "lightPurple";
+                        break;
+                    case 4:
+                        it.background = "yellow";
+                        break;
+
+                    case 5:
+                        it.background = "blue";
+                        it.span.col = 2;
+                        break;
+                }
+
+                results.push(it);
+            }
+            return results;
+        }
+
+        debugger;
+        $scope.tiles = $scope.buildGridModel({
+            //icon : "avatar:svg-",
+            title: "Svg-",
+            background: ""
+        });
+
     }]);
